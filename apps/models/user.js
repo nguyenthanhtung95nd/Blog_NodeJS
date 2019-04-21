6f4b1a13886a81c36f9b17372e4ff521937911d0
nguyenthanhtung95nd <nguyenthanhtung95nd@gmail.com>
2019-04-21 23:51:11 +0700

    - Add new fucntion delete - Use session in project

--- apps/models/user.js
+++ apps/models/user.js
@@ -38,7 +38,22 @@ function getUserByEmail(email) {
     return false;
 }
 
+function getAllUsers() {
+    var defer = q.defer();
+
+    var query = conn.query('SELECT * FROM users', function (err, users) {
+        if (err) {
+            defer.reject(err);
+        } else {
+            defer.resolve(users);
+        }
+    });
+
+    return defer.promise;
+}
+
 module.exports = {
     addUser: addUser,
-    getUserByEmail: getUserByEmail
+    getUserByEmail: getUserByEmail,
+    getAllUsers: getAllUsers
 }
